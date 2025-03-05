cef1b9a011c50501d3fdf8047055490a6040881e
anniethewebcoder <anniethewebcoder@gmail.com>
2025-03-04 20:42:12 -0500

    update

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,38 +1,64 @@
-const Job = require('./../models/Job')
-const parseVErr = require("./../util/parseValidationErr")
-const csrf = require("host-csrf")
+const Job = require("./../models/Job");
+const parseVErr = require("./../util/parseValidationErr");
+const csrf = require("host-csrf");
 
 const showForm = (req, res) => {
-    res.render("job")
-}
+  res.render("job", { job: null });
+};
 
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createdBy: req.user._id }).sort("createdAt");
-    res.render("jobs", { jobs, messages: req.flash() });
-}
+  const jobs = await Job.find({ createdBy: req.user._id }).sort("createdAt");
+  res.render("jobs", { jobs, messages: req.flash() });
+};
 
 const getJob = async (req, res) => {
-    const job = await Job.findById(req.params.id)
-    res.render("job", { job, messages: req.flash() });
-}
-const createJob = async (req, res) => {}
-const updateJob = async (req, res) => {
-    const job = await Job.findByIdAndUpdate(req.params.id, req.body)
-    res.rend("job", { job, messages: req.flash() })
+  const job = await Job.findById(req.params.id);
+  res.render("job", { job, messages: req.flash() });
+};
 
-}
-const deleteJob = async(req, res) => {
-    console.log(req.params.id)
-    let token = csrf.token(req, res);
-    const job = await Job.findByIdAndDelete(req.params.id)
-    res.send("job", { job, token, messages: req.flash() })
-}
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user._id;
+
+  const job = await Job.create(req.body);
+
+  res.render("job", { job: null });
+};
+
+const updateJob = async (req, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndUpdate(
+    {
+      _id: jobId,
+      createdBy: userId,
+    },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  //   res.render("job", { job, messages: req.flash() });
+  res.redirect("/jobs");
+};
+
+const deleteJob = async (req, res) => {
+  console.log(req.params.id);
+  let token = csrf.token(req, res);
+  const job = await Job.findByIdAndDelete(req.params.id);
+  res.render("job", { job, token, messages: req.flash() });
+};
 
 module.exports = {
-    getAllJobs,
-    getJob,
-    showForm,
-    createJob,
-    updateJob,
-    deleteJob
-}
+  getAllJobs,
+  getJob,
+  showForm,
+  createJob,
+  updateJob,
+  deleteJob,
+};
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,31 +1,30 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
-    getAllJobs,
-    getJob,
-    showForm,
-    createJob, 
-    updateJob, 
-    deleteJob
-} = require('./../controllers/jobs')
+  getAllJobs,
+  getJob,
+  showForm,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("./../controllers/jobs");
 
 // GET /jobs (display all the job listings belonging to this user)
-router.route("/").get(getAllJobs)
+router.route("/").get(getAllJobs);
 
 // POST /jobs (Add a new job listing)
-router.route("/").post(createJob)
-// GET /jobs/new (Put up the form to create a new entry)
+router.route("/").post(createJob);
 
-router.route("/new").post(showForm)
+// GET /jobs/new (Put up the form to create a new entry)
+router.route("/new").post(showForm);
 
 // GET /jobs/edit/:id (Get a particular entry and show it in the edit box)
-router.route("/edit/:id").get(getJob)
+router.route("/edit/:id").get(getJob);
 
 // POST /jobs/update/:id (Update a particular entry)
-router.route("/update/:id").post(updateJob)
+router.route("/update/:id").post(updateJob);
 
 // POST /jobs/delete/:id (Delete an entry)
-router.route("/delete/:id").post(deleteJob)
+router.route("/delete/:id").post(deleteJob);
 
-module.exports = router
-
+module.exports = router;
